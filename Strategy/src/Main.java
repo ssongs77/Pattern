235addfb66331465685bf96a2e3ee6694e4e6fdc
ssongs77 <ssongs77@gmail.com>
2017-09-14 17:58:47 +0900

    Interface 예제

---
+++ Strategy/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Ainterface ainterface = new AinterfaceImpl();
+		
+		//ainterface는 Ainterface를 통해 AinterfaceImpl의 funcA를 사용함
+		ainterface.funcA();
+	}
+
+}
